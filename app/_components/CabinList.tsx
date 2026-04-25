6ee45763adc3681512684125de1136075877ad1a
Mohammed-Mokhtar <mohamed.mokhtar.fai@gmail.com>
2026-04-25 22:44:55 +0300

    fix bug in cabinlist

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -13,13 +13,13 @@ export default async function CabinList({
   let displayedCabins = cabins;
 
   if (filter === "small")
-    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity! <= 3);
   if (filter === "medium")
     displayedCabins = cabins.filter(
-      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7,
+      (cabin) => cabin.maxCapacity! >= 4 && cabin.maxCapacity! <= 7,
     );
   if (filter === "large")
-    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity! >= 8);
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
